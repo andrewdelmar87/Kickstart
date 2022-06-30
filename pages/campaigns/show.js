11431f6d5360008f8499804851deb7d2b2769fd4
andrewdelmar87 <andrewjdelmar@gmail.com>
2022-06-30 15:35:08 -0700

    Campaign contribution

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
+import {Card, Grid} from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -9,6 +12,7 @@ class CampaignShow extends Component {
     const summary = await campaign.methods.getSummary().call();
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -17,10 +21,60 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description: "The manager is the camapign's creator and can make requests to withdraw money",
+        style: {overflowWrap: "break-word"}
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description: "Contribute at least this much wei to become an approver",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description: "A request tries to withdraw money from the contract. Requests must be approved by approvers."
+      },
+      {
+        header: approversCount,
+        meta: "Number of Approvers",
+        description: "Number of people who have already donated to this campaign."
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description: "The balance is how much money this campaign has to spend."
+      }
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
-        <h3>Campaign Show</h3>
+        <h3>Campaign View</h3>
+        <Grid>
+          <Grid.Column width={10}>
+            {this.renderCards()}
+          </Grid.Column>
+
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address} />
+          </Grid.Column>
+        </Grid>  
       </Layout>
     )
   }
