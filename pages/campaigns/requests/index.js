71826797603647e20bcb85b79a776aa8e3451fca
andrewdelmar87 <andrewjdelmar@gmail.com>
2022-07-04 19:00:07 -0700

    Request approval table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,14 +1,16 @@
 import React, {Component} from "react";
-import {Button} from "semantic-ui-react";
+import {Button, Table} from "semantic-ui-react";
 import {Link} from "../../../routes";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const {address} = props.query;
     const campaign = Campaign(address);
     const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
 
     const requests = await Promise.all(
       Array(parseInt(requestCount))
@@ -18,11 +20,27 @@ class RequestIndex extends Component {
       })
     );
 
-    return {address, requests, requestCount};
+    return {address, requests, requestCount, approversCount};
+  }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow 
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
   }
 
 
   render() {
+    const {Header, Row, HeaderCell, Body} = Table;
+
     return(
       <Layout>
         <h3>Requests</h3>
@@ -31,6 +49,22 @@ class RequestIndex extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount (ETH)</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>
+            {this.renderRows()}
+          </Body>
+        </Table>
       </Layout>
     );
   }
